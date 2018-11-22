71377f36a95a1fcbdd81e1c7767fd48697a2cf7d
Khushi55 <yadavkhushaboo50@gmail.com>
2018-11-22 15:03:06 +0530

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -12,9 +12,7 @@ def post_detail(request, pk):
 	
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
-def form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form}) 
+ 
 
 
 def post_new(request):
@@ -26,11 +24,11 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        else:
+          form = PostForm()
+          return render(request, 'blog/post_edit.html', {'form': form})
 
-    
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
